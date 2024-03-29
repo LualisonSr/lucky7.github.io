2b9e092584ab440405b909e9ad89106e5d63df4c
LualisonSr <111825827+LualisonSr@users.noreply.github.com>
2024-03-29 04:49:04 -0400

    Update drop_table.sql
    added updated triggers and subtable

--- database/drop_table.sql
+++ database/drop_table.sql
@@ -1,5 +1,10 @@
-DROP TABLE users;
-DROP TABLE categories;
-DROP TABLE products;
-DROP TABLE carts;
-DROP TABLE cartsProducts;
+-- Drop Triggers first to avoid unforseen errors
+DROP TRIGGER IF EXISTS update_cart_quantity_insert;
+DROP TRIGGER IF EXISTS update_cart_quantity_delete;
+
+DROP TABLE IF EXISTS CartsProductsSubquantities;
+DROP TABLE IF EXISTS CartsProducts;
+DROP TABLE IF EXISTS Carts;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Users;
