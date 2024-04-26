81e90d37f00cc6f40743e71b2b5f74df198077aa
LualisonSr <111825827+LualisonSr@users.noreply.github.com>
2024-04-26 03:23:47 -0400

    Update index.js

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,56 +1,46 @@
 let express = require('express');
 let router = express.Router();
-// let Database = require('../lucky.db');
 
-// Route for the home page
-router.get('/', (req, res) => {
+// Render home page
+router.get(['/', '/home'], (req, res) => {
     res.render('home');
 });
 
-// Additional route for '/home' that renders the same view as '/'
-router.get('/home', (req, res) => {
-    res.render('home');
-});
-
-// Route for the products page
+// Render products page
 router.get('/products', (req, res) => {
     res.render('products');
 });
 
-// Route for the cart page
+// Render cart page
 router.get('/cart', (req, res) => {
     res.render('cart');
 });
 
-// Route for the contact page
+// Render contact page
 router.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-// Route for the product details page
-router.get('/products/details', (req, res) => {
-    res.render('details');
+// Render product details page with parameterized route
+router.get('/products/:details', (req, res) => {
+    res.render('details', { detailId: req.params.details });
 });
 
-// Route for the admin main page
+// Admin routes
 router.get('/admin', (req, res) => {
     res.render('admin');
 });
 
-// Route for the bulk edit page under admin
 router.get('/admin/bulk', (req, res) => {
     res.render('bulk');
 });
 
-// Route for the product edit page under admin
 router.get('/admin/productEdit', (req, res) => {
     res.render('productEdit');
 });
 
-// Route for the admin signout page
 router.get('/admin/signout', (req, res) => {
     res.render('signout');
 });
 
-// Export the router so it can be used in server.js
 module.exports = router;
